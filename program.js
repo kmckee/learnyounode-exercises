2d208a2ba2fbecac0836935b859b4c60e137383e
Kyle McKee <mckee.kyle@gmail.com>
2015-08-20 21:01:00 -0400

    3

--- program.js
+++ program.js
@@ -1,6 +1,7 @@
-var sum = 0;
-for (var i = 2; i < process.argv.length; i++) {
-    var item = process.argv[i];
-    sum += Number(item);
-}
-console.log(sum);
+var fs = require('fs'),
+    filePath = process.argv[2],
+    fileBuffer = fs.readFileSync(filePath),
+    contents = fileBuffer.toString(),
+    lines = contents.split('\n'),
+    lineCount = lines.length - 1;
+console.log(lineCount);
