2a01619d272dfb87154692a4c8f606b3344607b1
Kyle McKee <mckee.kyle@gmail.com>
2015-08-20 21:09:02 -0400

    4

--- program.js
+++ program.js
@@ -1,7 +1,8 @@
 var fs = require('fs'),
-    filePath = process.argv[2],
-    fileBuffer = fs.readFileSync(filePath),
-    contents = fileBuffer.toString(),
+    filePath = process.argv[2];
+fs.readFile(filePath, function callback(err, buffer) {
+    var contents = buffer.toString(),
     lines = contents.split('\n'),
     lineCount = lines.length - 1;
-console.log(lineCount);
+    console.log(lineCount);
+});
