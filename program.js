49a73416ffdf272bce365f002fd8b5805b5a2df3
Kyle McKee <mckee.kyle@gmail.com>
2015-08-28 07:27:11 -0400

    9

--- program.js
+++ program.js
@@ -1,14 +1,23 @@
-var url = process.argv[2],
-    http = require('http');
+var urls = [process.argv[2], process.argv[3], process.argv[4]],
+    http = require('http'),
+    responses = [],
+    responseCount = 0;
 
-http.get(url, function (response) {
-    var content = "";
-    response.setEncoding('utf8');
-    response.on('data', function(data) {
-        content += data;
-    });
-    response.on('end', function() {
-        console.log(content.length);
-        console.log(content);
+urls.map(function(url, index) {
+    http.get(url, function (response) {
+        var content = "";
+        response.setEncoding('utf8');
+        response.on('data', function(data) {
+            content += data;
+        });
+        response.on('end', function() {
+            responseCount += 1;
+            responses[index] = content;
+            if (responseCount == urls.length) {
+                console.log(responses[0]);
+                console.log(responses[1]);
+                console.log(responses[2]);
+            }
+        });
     });
 });
