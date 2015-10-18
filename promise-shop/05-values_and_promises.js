dcf041d5274c53bc605db573474ee1a529679156
Kyle McKee <mckee.kyle@gmail.com>
2015-10-18 08:25:56 -0400

    Exercise 5 completed.

---
+++ promise-shop/05-values_and_promises.js
@@ -0,0 +1,11 @@
+var     q = require('q'),
+    defer = q.defer();
+
+function attachTitle (name) {
+    return "DR. " + name;
+}
+
+defer.promise.then(attachTitle)
+             .then(console.log);
+
+defer.resolve('MANHATTAN');
