b57c82188d65c0726a16a18d9f4f43331faf9870
Kyle McKee <mckee.kyle@gmail.com>
2015-12-06 12:26:11 -0500

    cleanup

--- functional_js/13_blocking_event_loop.js
+++ functional_js/13_blocking_event_loop.js
@@ -1,8 +1,5 @@
 function repeat(operation, num) {
-    // modify this so it can be interrupted
-    if (num <= 0) {
-        return;
-    }
+    if (num <= 0) { return; }
     operation();
     setTimeout(repeat.bind(this, operation, (num-1)));
 }
